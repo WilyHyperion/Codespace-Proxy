3ef379aa4ebb697897e6cb64fc097f14ce261e27
WilyHyperion <123993989+WilyHyperion@users.noreply.github.com>
2023-03-25 18:47:02 +0000

    Pending changes exported from your codespace

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 8000
 app.use(express.json())
 const axios = require('axios')
-
+console.log("Server is running");
 let CUrl = "";
 app.get("/" ,(req ,res) => {
     try {
@@ -12,17 +12,21 @@ app.get("/" ,(req ,res) => {
     } catch (error) {
         
     }
-    
 });
 
 app.get("*", (req, res) => {
-    console.log("GET " + CUrl + req.url );
-    try {
     let url = CUrl + req.url;
-    axios.get(url).then((resp) => {
-        res.send(resp.data);
+    console.log(url);
+    try{
+        axios({
+        method: 'GET',
+        url: url,
+        Headers: req.headers,
+    }).then((resp) => {
+            res.send(resp.data);
+        
     }).catch((error) => {
-        console.log("error");
+        console.log("error" + error.message);
     }
     );
 
@@ -32,16 +36,15 @@ app.get("*", (req, res) => {
 });
 
 app.post("/changeURL" ,(req ,res) => {
-
     try {
-    console.log("post");
     let body = req.body;
     let url ="https://" +  body.url;
-    CUrl = url;
+    CUrl = url.trim("/");
+    console.log("post, changing to:  " + CUrl);
     axios.get(url).then((resp) => {
         res.send(resp.data);
     }).catch((error) => {
-        console.log("error");
+        console.log(error.message);
     });
 
 } catch (error) {
@@ -49,9 +52,17 @@ app.post("/changeURL" ,(req ,res) => {
 
 });
 app.post("*", (req, res) => {
-    console.log(CUrl + req.url);
-    axios.post(CUrl + req.url, req.body
-        ).then((resp) => {
+    let u  =(CUrl + req.url)
+    console.log(u);
+    axios({
+        method: 'post',
+        url: u,
+        data: req.body,
+        Headers: req.headers,
+    }).then((resp) => {
+        for(let i in resp.headers){
+            res.setHeader(i, resp.headers[i]);
+        }
         res.send(resp.data);
     }).catch((error) => {
       console.log("error: " +  error.message);
